b8cfcc98b8be610d00a3f296a17aebcd794efdae
mask <momo617473294@gmail.com>
2024-02-23 08:34:08 +0800

    fix: 修改字典拦截器

--- src/system/dicts/dto/create-dict.dto.ts
+++ src/system/dicts/dto/create-dict.dto.ts
@@ -1,25 +1,33 @@
+import { IsNumber, IsString, MinLength } from 'class-validator';
+
 export class CreateDictDto {
   /**
    * 字典名称
    * @example '字典1'
    */
+  @IsString()
+  @MinLength(1)
   name: string;
 
   /**
    * 字典值
    * @example '1'
    */
+  @IsString()
+  @MinLength(1)
   value: string;
 
   /**
    * 字典描述
    * @example '这是一个字典'
    */
+  @IsString()
   remark?: string = '';
 
   /**
    * 模板id
    * @example 1
    */
+  @IsNumber()
   templateId: number;
 }
